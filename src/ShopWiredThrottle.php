ac157db93c36f751aab978dbba5493bb9efb0c9e
John Wedgbury <john@appoly.co.uk>
2020-09-07 16:17:59 +0100

    Update ShopWiredThrottle.php

--- src/ShopWiredThrottle.php
+++ src/ShopWiredThrottle.php
@@ -6,7 +6,7 @@ use Redis;
 
 class ShopWiredThrottle
 {
-    const BUCKET_SIZE = 40;
+    const BUCKET_SIZE = 30; // Originally 40
     const DRAIN_PER_SECOND = 2;
 
     private static $enabled = true;
